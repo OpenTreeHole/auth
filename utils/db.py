611d849c0a64d2382a7ee312928e5ea06beefff4
嘉然今天吃什么 <jsclndnz@outlook.com>
2022-02-21 13:56:40 +0800

    get_object_or_404

--- utils/db.py
+++ utils/db.py
@@ -4,5 +4,5 @@ from sanic.exceptions import NotFound
 async def get_object_or_404(klass, *args, **kwargs):
     instance = await klass.get_or_none(*args, **kwargs)
     if not instance:
-        raise NotFound(f'{klass} does not exist')
+        raise NotFound(f'{klass.__name__} does not exist')
     return instance
