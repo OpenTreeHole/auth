1e4c99746762a5ff977715fea1b5c910c5c9c664
嘉然今天吃什么 <jsclndnz@outlook.com>
2022-02-21 12:14:21 +0800

    重构

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
 import multiprocessing
 
-from sanic import Sanic
-from sanic.log import logger
+from sanic import Sanic, json
 
 app = Sanic('auth')
+from sanic.exceptions import Unauthorized
+from sanic.log import logger
+from sanic_ext import validate
+
+from models import User
+from serializers import LoginSerializer
+from utils.auth import many_hashes, check_password
+from utils.db import get_object_or_404
 
 if app.config.get('DEBUG', True):
     logger.warning('\nserver is running in dev mode, do not use in production\n')
 
-if __name__ == '__main__':
-    from apis import app as server
 
-    server.run(
+@app.get('/')
+async def home(request):
+    return json({'message': 'hello world'})
+
+
+@app.post('/login')
+@validate(json=LoginSerializer)
+async def login(request, body: LoginSerializer):
+    user = await get_object_or_404(User, identifier=many_hashes(body.email))
+    if not check_password(body.password, user.password):
+        raise Unauthorized('password incorrect')
+    return json({'login': True})
+
+
+@app.post('/register')
+async def register(request):
+    return json({})
+
+
+if __name__ == '__main__':
+    app.run(
         host='0.0.0.0',
         port=8000,
         workers=app.config.get('WORKERS', multiprocessing.cpu_count()),
