2ef230cac700c016a22db21d4e14766a95a14e11
嘉然今天吃什么 <jsclndnz@outlook.com>
2022-03-20 11:17:51 +0800

    kong token，测试连接

--- utils/kong.py
+++ utils/kong.py
@@ -5,13 +5,18 @@ import httpx
 from pydantic import BaseModel
 from sanic import Sanic
 from sanic.exceptions import ServerError
+from sanic.log import logger
 
 app = Sanic.get_app()
 KONG_URL = app.config.get('KONG_URL', 'http://kong:8001')
+KONG_TOKEN = app.config.get('KONG_TOKEN', '')
+headers = {
+    'Authorization': KONG_TOKEN
+} if KONG_TOKEN else {}
 
 
 async def create_user(id: int) -> dict:
-    async with httpx.AsyncClient(base_url=KONG_URL) as c:
+    async with httpx.AsyncClient(base_url=KONG_URL, headers=headers) as c:
         r = await c.put(f'/consumers/{id}')
         if not r.status_code == 200:
             raise ServerError(r.json().get('message'), extra=r.json())
@@ -26,7 +31,7 @@ class JwtCredential(BaseModel):
 
 
 async def create_jwt_credential(id: int) -> JwtCredential:
-    async with httpx.AsyncClient(base_url=KONG_URL) as c:
+    async with httpx.AsyncClient(base_url=KONG_URL, headers=headers) as c:
         r = await c.post(f'/consumers/{id}/jwt')
         if not r.status_code == 201:
             raise ServerError(r.json().get('message'), extra=r.json())
@@ -34,7 +39,7 @@ async def create_jwt_credential(id: int) -> JwtCredential:
 
 
 async def list_jwt_credentials(id: int) -> List[dict]:
-    async with httpx.AsyncClient(base_url=KONG_URL) as c:
+    async with httpx.AsyncClient(base_url=KONG_URL, headers=headers) as c:
         r = await c.get(f'/consumers/{id}/jwt')
         if not r.status_code == 200:
             raise ServerError(r.json().get('message'), extra=r.json())
@@ -44,8 +49,8 @@ async def list_jwt_credentials(id: int) -> List[dict]:
 
 async def get_jwt_credential(id: int) -> JwtCredential:
     """
-    返回用户的第一个 credential
-    """
+                返回用户的第一个 credential
+                """
     data = await list_jwt_credentials(id)
     if len(data) == 0:
         return await create_jwt_credential(id)
@@ -56,7 +61,7 @@ async def delete_jwt_credentials(id: int) -> int:
     data = await list_jwt_credentials(id)
     num = 0
     tasks = []
-    async with httpx.AsyncClient(base_url=KONG_URL) as c:
+    async with httpx.AsyncClient(base_url=KONG_URL, headers=headers) as c:
         for i in data:
             tasks.append(c.delete(f'/consumers/{id}/jwt/{i["id"]}'))
         results = await asyncio.gather(*tasks)
@@ -64,3 +69,14 @@ async def delete_jwt_credentials(id: int) -> int:
             if r.status_code == 204:
                 num += 1
         return num
+
+
+async def connect_to_gateway():
+    async with httpx.AsyncClient(base_url=KONG_URL, headers=headers) as c:
+        r = await c.get('/')
+        if not r.status_code == 200:
+            logger.error('Kong API gateway unreachable!')
+        logger.info('gateway connected')
+
+
+asyncio.run(connect_to_gateway())
