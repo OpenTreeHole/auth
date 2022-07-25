7456e57cc07fef44134a55c294e06bdc577ae488
顶碗人 <jsclndnz@outlook.com>
2022-07-25 15:10:27 +0800

    change: route /api prefix

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 app = FastAPI()  # app 实例化位于所有导入之前
 
 from admin import punishment, user
 from auth import account, token
 
-app.include_router(account.router)
-app.include_router(token.router)
-app.include_router(punishment.router)
-app.include_router(user.router)
+app.include_router(account.router, prefix='/api')
+app.include_router(token.router, prefix='/api')
+app.include_router(punishment.router, prefix='/api')
+app.include_router(user.router, prefix='/api')
+
+
+@app.get('/api')
+async def home():
+    return {'message': 'hello world'}
 
 
 @app.get('/')
-async def home():
-    return {'message': 'hello world'}
+async def redirect_to_home():
+    return RedirectResponse('/api')
